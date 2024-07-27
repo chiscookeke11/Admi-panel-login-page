a4b88a2d864364405e29fd0a6751f849188f17ac
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2024-07-27 04:34:29 -1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function checkLogin(event) {
+    event.preventDefault(); 
+    
+    const username = document.getElementById('email-text').value;
+    const password = document.getElementById('Password-text').value;
+    const report = document.getElementById('report');
+
+    const validUsername = 'chinedu';
+    const validPassword = 'passme';
+
+    if (username.length < 1 || password.length < 1 ) {
+        report.innerHTML = "Please Input Your Login Details";
+    } else if (username === validUsername && password === validPassword) {
+        window.location.href = 'admin panel.htm';
+    } else {
+        report.innerHTML = "Invalid Login Details";
+    }
+}
+
+document.getElementById('loginForm').addEventListener('submit', checkLogin);
+;
+let rocketLink = document.getElementById("rocket-link");
+let arrowLink = document.getElementById("arrow-link");
+
+console.log(arrowLink);
+
